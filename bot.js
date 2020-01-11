18e8b75119583cc6946c286eb64452e3ab2ac0b6
pleximar <account@meier-ilvesheim.de>
2020-01-11 13:57:43 +0100

    First commit

---
+++ bot.js
@@ -0,0 +1,48 @@
+const tmi = require("tmi.js")
+const secrets = require("./config.json");
+var prefix = "?"
+
+
+var config = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: (secrets.cluster),
+        reconnect: true
+    },
+    identity: {
+        username: (secrets.username),
+        password: (secrets.password)
+    },
+    channels: [(secrets.channels)]
+}
+
+var client = new tmi.client(config)
+client.connect();
+console.log('============================')
+console.log(' ____   ___________ _______')
+console.log(' |__] \_/  | |___|__]|  | | ')
+console.log(' |__]  |  | |___|__]|__| | ')
+console.log('')
+console.log('============================')
+console.log()
+client.on("connected", (address, port) => {
+    client.action("The bot has connected on" + address + ":" + port)
+})
+
+
+
+client.on("chat", (channel, user, message, self) => {
+    if (self) return;
+
+    const args = message.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    try {
+        let commandFile = require(`./commands/${cmd}.js`)
+        commandFile.run(client, message, args, user, channel, self)
+    } catch (err) {
+        return;
+    }
+
+})
---
+++ commands/discord.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args, user, channel, self) => {
+    client.say(channel, `TwitchLit | Discord von der Community für die Community: https://discord.gg/ufjwGsp`)
+}
